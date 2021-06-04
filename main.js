62a6308db4ed8b4902b146addf8e682cfc64c583
J-44 <74297043+J-44@users.noreply.github.com>
2021-06-04 20:41:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+    img = loadImage("272-2720888_bird-cartoon-drawing-clip-art-animated-picture-of-bird.png");
+    }
+    function setUp(){
+    canvas = createCanvas(300, 300);
+        canvas.center();
+    }
+    function draw(){
+        circle(90,10,20);
+    }
+    Webcam.set({
+        width : 300,
+        height : 300,
+        image_format : 'png',
+        png_quality : 90
+    });
+    cam = document.getElementById("cam");
+    Webcam.attach('#cam');
